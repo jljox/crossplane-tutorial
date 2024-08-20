48a97c79a93c1c5c0c64a564e9c4607f0a164901
Jean-Luc Jox <jeanluc.jox@gmail.com>
2024-08-20 12:24:04 +1000

    using nix flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        nativeBuildInputs = [
+          pkgs.gum
+          pkgs.gh
+          pkgs.kind
+          pkgs.kubectl
+          pkgs.yq-go
+          pkgs.jq
+          pkgs.awscli2
+          pkgs.upbound
+          pkgs.teller
+          pkgs.kubernetes-helm
+        ];
+      };
+    });
+}
